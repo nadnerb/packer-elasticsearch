ce801c09847415c36a849f7b0d12a176ba8d4be8
Brendan Spinks <brendanspinks@gmail.com>
2015-07-28 16:42:23 +1000

    install elastic-head

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -10,10 +10,8 @@ sudo dpkg -i elastic.deb
 rm elastic.deb
 sudo update-rc.d elasticsearch defaults
 
-# previously uploaded
-sudo mv /tmp/elasticsearch.yml /etc/elasticsearch/
-
 cd /usr/share/elasticsearch
 sudo chown elasticsearch:elasticsearch -R .
 sudo bin/plugin -i elasticsearch/elasticsearch-cloud-aws/2.7.0
-sudo bin/plugin -i elasticsearch/marvel/latest
+#sudo bin/plugin -i elasticsearch/marvel/latest
+sudo bin/plugin -i mobz/elasticsearch-head
