e61998de622248b48954e5325bd93f2dc1da2a5f
infrascode <brendans@pagueppeople.com>
2016-02-02 03:13:25 +0000

    updated java version

--- scripts/java.sh
+++ scripts/java.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.rpm"
-sudo yum localinstall -y jdk-8u66-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u71-b15/jdk-8u71-linux-x64.rpm" -O jdk-8.rpm
+yum localinstall -y jdk-8.rpm
 
 echo "Check installation"
 java -version
