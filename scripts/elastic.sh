5ca5e127879d92a5b02a1d3b7d0f5b74914434ef
Brendan Spinks <brendanspinks@gmail.com>
2016-04-18 17:03:53 +1000

    removed reference to check.py

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# not used anymore.. will be upgrading to 2.1 asap
-elasticsearch_version=$ELASTIC_VERSION
-
 cd /tmp
 curl -L -o elastic.rpm https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/${ELASTIC_VERSION}/elasticsearch-${ELASTIC_VERSION}.rpm
 sudo rpm -Uvh elastic.rpm
@@ -17,9 +14,6 @@ sudo chown elasticsearch:elasticsearch -R .
 sudo yum install -y expect
 sudo chmod +x /tmp/config/awsplugin.sh
 /tmp/config/awsplugin.sh
-
-echo "elasticsearch health check"
-mv /tmp/config/check.py /etc/consul.d/
 
 echo "elasticsearch config"
 sudo mkdir -p /etc/elasticsearch/configurable
